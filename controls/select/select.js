ce8599926dbab9f02dd63ba708dcc687558eef61
Sahak Ter-Adamyan <sahak.teradamyan@volo.global>
2020-08-11 17:59:00 +0300

    add new

--- controls/select/select.js
+++ controls/select/select.js
@@ -1,10 +1,10 @@
 export default class Select {
   #el$;
+  #options;
 
   constructor(selector, options = {}) {
-    const { placeholder, items } = options;
     this.#el$ = document.querySelector(selector);
-    this.#el$.innerHTML = Select.#renderTemplate(options.placeholder);
+    this.#el$.innerHTML = Select.#renderTemplate(options.placeholder, options.data);
 
     this.#el$.addEventListener('click', () => {
       this.#toggleSelect();
@@ -13,15 +13,16 @@ export default class Select {
     console.dir(this.#el$.innerHTML);
   }
 
-  static #renderTemplate(placeholder = 'Choose something') {
+  static #renderTemplate(placeholder = 'Choose something', options) {
+    options = options.map((item) => {
+      return `<div class="select__option" data-value="${item.value}">${item.label}</div>`;
+    });
+    console.log(options);
     return `
       <div class="select">
         <div class="select__placeholder">${placeholder}</div>
         <div class="select__options">
-          <div class="select__option">1</div>
-          <div class="select__option">2</div>
-          <div class="select__option">3</div>
-          <div class="select__option">4</div>
+          ${options.join('')}
         </div>
       </div>
     `;
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-import Select from './controls/select.js'
+import Select from './controls/select/select.js'
 const selectOptions = {
-  placeholder: 'Select cars'
+  placeholder: 'Select cars',
+  data: [
+    {
+      label: 'Angular',
+      value: 'angular_value'
+    },
+    {
+      label: 'VueJS',
+      value: 'vue_value'
+    },
+    {
+      label: 'ReactJS',
+      value: 'react_value'
+    },
+    {
+      label: 'Swelte',
+      value: 'swelte_value'
+    },
+  ]
 };
 const select = new Select('.form__control--select', selectOptions);
 // select.
